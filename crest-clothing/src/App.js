30dfcc2260f469dbf2fafb71ccb9f9624734a6e1
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-07-11 18:26:36 -0400

    reinstalled yarn.lock to fix error. added react-router. setting up routes

--- crest-clothing/src/App.js
+++ crest-clothing/src/App.js
@@ -1,11 +1,16 @@
-import '../src/categories.styles.scss';
-import CategoryDirectory from './components/category-directory/category-directory.component';
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./routes(Pages)/home/home.component";
+
+//Routes registers, 'Route' level components, that will render specific components(elements) 
+//when it matches the specific route you're looking for. It matches through a specific 'path' variable,
+//when the route is matched to the 'path' value, whatever is in the 'element' component is shown.
 function App() {
-
   return (
-
-   <CategoryDirectory />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+   
   );
 }
 
